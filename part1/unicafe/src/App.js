8980c3755608db8cc68181d91161a8d87a72843f
I Chun Hurng <ichunhurng@gmail.com>
2022-10-27 13:08:51 -0700

    Add initial rating buttons and display for user to provide feedback

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Button = (prop) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button onClick={prop.click}>{prop.name}</button>
+  )
+  
 }
 
-export default App;
+const Display = (prop) => {
+  return (
+    <div>{prop.name}</div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button click={() => setGood(good + 1)} name="good"/>      
+      <Button click={() => setNeutral(neutral + 1)} name="neutral"/>
+      <Button click={() => setBad(bad + 1)} name="bad"/>
+      
+      <h1>statistics</h1>
+      <Display name={"good "+good}/>
+      <Display name={"neutral "+neutral}/>
+      <Display name={"bad "+bad}/>
+    </div>
+  )
+}
+
+export default App
