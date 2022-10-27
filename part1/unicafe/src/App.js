e6a0fc77b1b740679de83c03a4ea4937d728871d
I Chun Hurng <ichunhurng@gmail.com>
2022-10-27 14:11:28 -0700

    Add additional statistical calculations

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,21 +15,66 @@ const Display = (prop) => {
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [statistics, setStatistics] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    all: 0,
+    total: 0,
+    average: 0,
+    positive: 0,
+  })
+
+  const addToGood = () =>{
+    const newStatistics = {
+      ...statistics,
+      good: statistics.good + 1,
+      all: statistics.all + 1,
+      total: statistics.total +1,
+      average: (statistics.total+1)/(statistics.all+1),
+      positive: (statistics.good+1)/(statistics.all+1)
+    }
+    setStatistics(newStatistics)
+  }
+
+  const addToNeutral = () =>{
+    const newStatistics = {
+      ...statistics,
+      neutral: statistics.neutral + 1,
+      all: statistics.all + 1,
+      average: (statistics.total)/(statistics.all+1),
+      positive: statistics.good/(statistics.all+1)
+    }
+    setStatistics(newStatistics)
+  }
+
+  const addToBad = () =>{
+    const newStatistics = {
+      ...statistics,
+      bad: statistics.bad + 1,
+      all: statistics.all + 1,
+      total: statistics.total - 1,
+      average: (statistics.total - 1)/(statistics.all+1),
+      positive: statistics.good/(statistics.all+1)
+    }
+    setStatistics(newStatistics)
+  }
+
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button click={() => setGood(good + 1)} name="good"/>      
-      <Button click={() => setNeutral(neutral + 1)} name="neutral"/>
-      <Button click={() => setBad(bad + 1)} name="bad"/>
+      <Button click={addToGood} name="good"/>      
+      <Button click={addToNeutral} name="neutral"/>
+      <Button click={addToBad} name="bad"/>
       
       <h1>statistics</h1>
-      <Display name={"good "+good}/>
-      <Display name={"neutral "+neutral}/>
-      <Display name={"bad "+bad}/>
+      <Display name={"good "+statistics.good}/>
+      <Display name={"neutral "+statistics.neutral}/>
+      <Display name={"bad "+statistics.bad}/>
+      <Display name={"all "+statistics.all}/>
+      <Display name={"average "+statistics.average}/>
+      <Display name={"positive "+statistics.positive}/>
     </div>
   )
 }
